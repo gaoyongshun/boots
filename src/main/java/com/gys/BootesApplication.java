83c4f2e014a10ca8d1a4366e034be59cd2da0027
gaoyongshun <903357035@qq.com>
2020-09-15 14:50:33 +0800

    22

--- src/main/java/com/gys/BootesApplication.java
+++ src/main/java/com/gys/BootesApplication.java
@@ -1,12 +1,7 @@
 package com.gys;
 
-import com.alibaba.nacos.api.config.annotation.NacosValue;
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
