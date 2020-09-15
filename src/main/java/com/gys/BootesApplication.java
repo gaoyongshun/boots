18b1c01981fe7702738e4a9c776cc9b5f2e85837
gaoyongshun <903357035@qq.com>
2020-09-15 15:26:49 +0800

    333

--- src/main/java/com/gys/BootesApplication.java
+++ src/main/java/com/gys/BootesApplication.java
@@ -2,7 +2,11 @@ package com.gys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @SpringBootApplication
 @RestController
@@ -11,5 +15,9 @@ public class BootesApplication {
         SpringApplication.run(BootesApplication.class,args);
     }
 
+    @RequestMapping("/index")
+    public void index(HttpServletResponse response) throws IOException {
+        response.getOutputStream().println("hello world");
+    }
 
 }
